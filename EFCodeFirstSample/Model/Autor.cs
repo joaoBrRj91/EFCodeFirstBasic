593563c84b35e25f4d25a6d92f12806707e41295
joao nascimento <joao1991algoritmo@gmail.com>
2018-08-22 23:12:22 -0300

    Criação das entidades e dos relacionamentos

---
+++ EFCodeFirstSample/Model/Autor.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+
+namespace EFCodeFirstSample.Model
+{
+    public class Autor
+    {
+        public Autor()
+        {
+            Livros = new List<Livro>();
+        }
+
+        public  int Id { get; set; }
+        public  string Nome { get; set; }
+        public virtual ICollection<Livro> Livros { get; set; }
+    }
+}
