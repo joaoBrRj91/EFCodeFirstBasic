51c11cee268942d16893d475ddeb54a72162f138
joao nascimento <joao1991algoritmo@gmail.com>
2018-08-22 23:38:46 -0300

    Criação do contexto e configuração

---
+++ EFCodeFirstSample/DataContext/LivrariaDbContext.cs
@@ -0,0 +1,31 @@
+﻿
+using EFCodeFirstSample.Model;
+using System.Data.Entity;
+
+namespace EFCodeFirstSample.DataContext
+{
+    public class LivrariaDbContext : DbContext
+    {
+        public LivrariaDbContext() : base("LivrariaDbContext")
+        {
+            Database.SetInitializer(new LivrariaDbContextInitilizer());
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+
+    public class LivrariaDbContextInitilizer : DropCreateDatabaseAlways<LivrariaDbContext>
+    {
+        
+    }
+}
